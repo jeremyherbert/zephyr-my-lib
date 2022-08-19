95d239aa5b07e19c113a27f9dcafc3b9b66283fe
Jeremy Herbert <jeremy.006@gmail.com>
2022-08-20 09:58:11 +1000

    initial commit

---
+++ src/my_lib.c
@@ -0,0 +1,5 @@
+#include "my_lib.h"
+
+int my_lib_add_numbers(int a, int b) {
+    return a + b;
+}
---
+++ src/my_lib.h
@@ -0,0 +1,6 @@
+#ifndef __MY_LIB_H__
+#define __MY_LIB_H__
+
+int my_lib_add_numbers(int a, int b);
+
+#endif
